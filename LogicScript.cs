cf9af544ebb206fc9cdcaed8020eaf8164706363
3lejon <Rodny.engblom@gmail.com>
2025-12-11 12:29:31 +0200

    Create LogicScript.cs

---
+++ LogicScript.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class logicscript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+
+    void Start()
+    {
+        // Hide the game over screen at the start
+        gameOverScreen.SetActive(false);
+    }
+
+    [ContextMenu("increase Score")] 
+    public void addscore(int scoreToAdd)
+    {
+        playerScore = playerScore + scoreToAdd;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void restartgame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameover()
+    {
+        // Show the game over screen when the game ends
+        gameOverScreen.SetActive(true);
+    }
+}
+
+
